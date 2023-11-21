ebb1ea7b9172c6321fd3ba4c0139d4ef717c376c
arijitswain23 <arijitswain369@gmail.com>
2023-11-21 13:54:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def india(request):
+    return render(request,'india.html')
+
+def austrelia(request):
+    return render(request,'austrelia.html')
+
+def virat(request):
+    return render(request,'viart.html')
